704e8bdabd20f192541c5f63237e604649e3590f
Seungho <hsh47607@naver.com>
2023-05-01 16:28:34 +0900

    delete bookmark

--- src/test/java/service/applicationservice/bookmark/BookmarkDeleteServiceImplTest.java
+++ src/test/java/service/applicationservice/bookmark/BookmarkDeleteServiceImplTest.java
@@ -1,7 +1,36 @@
 package service.applicationservice.bookmark;
 
+import global.config.InstanceFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import service.controller.dto.BookmarkSaveRequestDto;
+import service.controller.dto.BookmarkSaveResponseDto;
+import service.repository.BookmarkRepository;
+
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BookmarkDeleteServiceImplTest {
+
+    private final BookmarkSaveService bookmarkSaveService = InstanceFactory.BookmarkSaveServiceFactory.getInstance();
+    private final BookmarkDeleteService bookmarkDeleteService = InstanceFactory.BookmarkDeleteServiceFactory.getInstance();
+    private final BookmarkRepository bookmarkRepository = InstanceFactory.BookmarkRepositoryFactory.getInstance();
+
+    @Test
+    void test_deleteBookmark() {
+        BookmarkSaveResponseDto save = bookmarkSaveService.saveBookmark(BookmarkSaveRequestDto.of("북마크 삭제", 10));
+
+        bookmarkDeleteService.deleteBookmark(save.getId());
+
+        Assertions.assertEquals(Optional.empty(), bookmarkRepository.findById(save.getId()));
+    }
+
+    @Test
+    void test_deleteNotExistsBookmark() {
+
+        Assertions.assertThrows(RuntimeException.class, () -> bookmarkDeleteService.deleteBookmark(100000L));
+
+    }
 
 }
